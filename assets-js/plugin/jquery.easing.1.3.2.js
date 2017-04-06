5602254d073da4c343ae3727db1f4a9c68270f34
lacasebiocoutras <lacasebiocoutras.adm@gmail.com>
2017-04-06 19:29:33 +0000

    build

--- assets-js/plugin/jquery.easing.1.3.2.js
+++ assets-js/plugin/jquery.easing.1.3.2.js
@@ -1,1 +1,1 @@
-!function(n){n.easing.jswing=n.easing.swing,n.extend(n.easing,{def:"easeOutQuad",swing:function(e,i,a,s,t){return n.easing[n.easing.def](e,i,a,s,t)},easeInOutExpo:function(n,e,i,a,s){return 0==e?i:e==s?i+a:(e/=s/2)<1?a/2*Math.pow(2,10*(e-1))+i:a/2*(-Math.pow(2,-10*--e)+2)+i}})}(jQuery);
+!function(n){n.easing.jswing=n.easing.swing,n.extend(n.easing,{def:"easeOutQuad",swing:function(e,i,a,s,t){return n.easing[n.easing.def](e,i,a,s,t)},easeInOutExpo:function(n,e,i,a,s){return 0==e?i:e==s?i+a:(e/=s/2)<1?a/2*Math.pow(2,10*(e-1))+i:a/2*(2-Math.pow(2,-10*--e))+i}})}(jQuery);
